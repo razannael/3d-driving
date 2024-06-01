836ca726d52c77603a82bfedf2b3a06f20e74d27
razannael <nailrazan40@gmail.com>
2024-06-01 18:27:57 +0300

    init

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+import * as THREE from 'three';
+import * as YUKA from 'yuka';
+import gsap from 'gsap';
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+
+// Camera positioning
+camera.position.set(6, 8, 14);
+orbit.update();
+
+// Sets a 12 by 12 gird helper
+const gridHelper = new THREE.GridHelper(12, 12);
+scene.add(gridHelper);
+
+// Sets the x, y, and z axes with each having a length of 4
+const axesHelper = new THREE.AxesHelper(4);
+scene.add(axesHelper);
+
+function animate() {
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
